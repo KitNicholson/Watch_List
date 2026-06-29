1dc26272cf7ddc34937a23f1e6a635d746fc4f47
Kit Nicholson <kitnicholson@protonmail.com>
2026-06-29 14:34:52 +1000

    tweaks and text alignment

--- scripts.js
+++ scripts.js
@@ -6,18 +6,12 @@ function test() {
 
 function toggleText(text_ID) {
 
-    console.log('toggle');
-
     var text = document.getElementById(text_ID);
-    console.log(text.style.display);
-
 
     if (text.style.display === 'block') {
         text.style.display = 'none';
     } else {
         text.style.display = 'block';
     }
-
-    console.log(text.style.display);
 
 }
